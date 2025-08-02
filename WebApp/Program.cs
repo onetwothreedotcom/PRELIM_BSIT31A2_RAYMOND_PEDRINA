c284be7a1815827f64bdd85b445f4eb2d932eea5
RAYMOND PEDRIÑA <C1744-23@itmlyceumalabang.onmicrosoft.com>
2025-08-02 12:43:48 +0800

    home page

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+    pattern: "{controller=Library}/{action=Index}/{id?}");
 
 app.Run();
